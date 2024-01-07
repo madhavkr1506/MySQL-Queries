390ee4a970d7c9ab633f5300475a5e861c54cace
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-01-07 15:45:48 +0530

    Add files via upload

--- MySQL Queries/Database_Query1.sql
+++ MySQL Queries/Database_Query1.sql
@@ -72,4 +72,56 @@ constraints : sql constraints are used to specifies rules for data in a table;
 1. not null : columns cannot have null values;
 2. unique : all values in column are different;
 3. primary key : make a column unique and not null used only for one;
+4. foreign key : prevent actions that would destroy links between tables;
+5. default : set the default value of a column;  eg : salary int default 25000;
+6. check : it can limit the values allowed in the column;
 */
+
+
+create table temp1(Id int unique);
+insert into temp1 values(101);
+-- insert into temp1 values(101); It will throw an error because it is a duplicate entry;
+select * from temp1;
+
+-- different syntax for using primary key; 
+create table temp2 (Id int ,name varchar(50),primary key(id));
+create table temp3(Id int, name varchar(50),primary key(id,name));
+
+-- syntax for foreign key; 
+create table temp4 (Id int,foreign key(Id) references temp2(Id));
+drop table temp4;
+create table temp5(Id int , foreign key(Id) references temp3(Id));
+
+-- syntax for default; 
+create table temp6(Id int, salary int default 25000);
+insert into temp6 values (12213356,30000);
+insert into temp6 (Id) values (12213356);
+-- insert into temp6 values (12213356,30000),(12213356);  It will throw an error;
+select * from temp6;
+
+-- syntax for check;
+create table temp7(Id int primary key,city varchar(50),age int,constraint age_check check(age >=18 and city ="patna"));
+insert into temp7 values (12213356,"patna",20);
+-- insert into temp7 values (12213357,"up",17); It will throw an error because constraint is violated;
+select * from temp7;
+
+create table temp8(age int check (age >= 18));
+
+-- select command : It is used to select any data from the database;
+create table temp9(Id int ,name varchar(50),country varchar(50),salary int, primary key(Id));
+insert into temp9 values(12213356,"Madhav","India",56100),(12213357,"Ram jee","India",56200),(12213358,"Sandeep","India",56300),(12213359,"Happy","India",56400);
+select * from temp9;
+select name from temp9;
+select Id from temp9;
+select country from temp9;
+select salary from temp9;
+select name,salary from temp9;
+
+
+
+
+
+
+
+
+
